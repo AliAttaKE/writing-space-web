ba70a9ee06a159043242307de38503e9bac7d363
shariqiqbal571 <shariqiqbal571@gmail.com>
2025-04-17 23:10:57 +0500

    chk

--- app/Http/Controllers/Backend/Customer/OrderManagement/CustomerPlaceOrderController.php
+++ app/Http/Controllers/Backend/Customer/OrderManagement/CustomerPlaceOrderController.php
@@ -2638,8 +2638,8 @@ Mail::html($emailContent, function ($message) use ($user) {
             $subsDetails = User_Subscription::where('user_id', $user_id)->first();
             $subscribed = $subsDetails->user_id ?? '';
 
-            dd($subscribed , $subsDetails->rollover_pages, $subsDetails->remaining_pages);
-            if ($subscribed && $subsDetails->rollover_pages != 0 && $subsDetails->remaining_pages != 0) {
+        //   dd($subscribed , $subsDetails->rollover_pages, $subsDetails->remaining_pages);
+            if ($subscribed || $subsDetails->rollover_pages != 0 || $subsDetails->remaining_pages != 0) {
                 $subsDetailsamount = Subscription::where('id', $subsDetails->subscription_id)->first();
                 $cost_per_page = $subsDetailsamount->cost_per_page;
                 return view('backend.customer.orderManagement.custom_place_order', compact('Languages','used_subscription','Addons', 'pricing', 'subjects', 'academic', 'term', 'deadline', 'paper_format', 'subscribed', 'subsDetails', 'cost_per_page'));
