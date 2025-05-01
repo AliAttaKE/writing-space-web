a0b370b20104edac474d496e0f138a4c638aa5c9
shariqiqbal571 <shariqiqbal571@gmail.com>
2025-05-01 16:17:34 +0500

    admin send message customer

--- app/Http/Controllers/Backend/Admin/Message/MessageController.php
+++ app/Http/Controllers/Backend/Admin/Message/MessageController.php
@@ -230,25 +230,29 @@ public function sendMessage(Request $request)
         'message_content' => $request->message
     ];
 
-    // Inline email content
+    $senderRole = ($input['statusRadio'] == 'Admin') ? 'Admin' : 'Writer';
+    $recipientName = auth()->user()->name;
+
     $emailContent = "
     <html>
         <body>
             <p>Hi,</p>
             <p>We're keeping the lines of communication open at Writing Space! You've just received a new message in your account. Here’s what you need to know:</p>
-            <p><strong>From:</strong> {$data['sender_role']}<br>
+            <p><strong>From:</strong> {$senderRole}<br>
+              <strong>To:</strong> Customer<br>
                <strong>Order ID:</strong> {$data['order_id']}<br>
                <strong>Message:</strong> <br>{$data['message_content']}</p>
             <p><strong>What’s Next?</strong></p>
-            <p>
-                We encourage you to review the message and respond if necessary. Staying engaged and communicating effectively is key to making the most of our services and ensuring your academic journey is smooth.<br>
-                You can reply directly through your dashboard under the “Messages” section. It’s easy, fast, and secure.
-            </p>
-            <p>If you have any questions or need further assistance, our support team is just an email away. We're here to help you succeed!</p>
-            <p>Thanks for choosing Writing Space as your trusted academic partner.</p>
-            <p>Warm regards,<br>
-            Customer Success Team<br>
-            Writing Space</p>
+            <p><strong>Next Steps:</strong></p>
+                <p>
+                    We encourage you to review the message and respond if necessary. Staying engaged and communicating effectively is key to making the most of our services and ensuring your academic journey is smooth.
+                </p>
+                <p>You can reply directly through your dashboard under the “Messages” section. It’s easy, fast, and secure.</p>
+                <p>If you have any questions or need further assistance, our support team is just an email away. We're here to help you succeed!</p>
+                <p>Thanks for choosing Writing Space as your trusted academic partner.</p>
+                <p>Warm regards,<br>
+                Customer Success Team<br>
+                Writing Space</p>
         </body>
     </html>";
 
