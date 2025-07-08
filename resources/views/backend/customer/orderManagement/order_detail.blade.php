886ff8743e924a73d9131d04a10dca3dd7e4e4b6
shariqiqbal571 <shariqiqbal571@gmail.com>
2025-07-08 23:44:31 +0500

    order details set file setting

--- resources/views/backend/customer/orderManagement/order_detail.blade.php
+++ resources/views/backend/customer/orderManagement/order_detail.blade.php
@@ -1082,7 +1082,12 @@ button.btn.btn-flex.badge-custom-bg.w-100.justify-content-center.px-2.ms-3.downl
 																				@auth
 																					<td class="w-25">{{ $file->Writer ?? Auth::user()->role }}</td>
 																				@endauth
-																				<td class="w-25">Writer</td>
+																				@if($file->Writer != 'Admin')
+																					<td class="text-white">writer</td>
+																														@else
+
+																					<td class="text-white">Customer</td>
+																														@endif
 
 																				<td class="text-end text-white" data-kt-filemanager-table="action_dropdown">
 																					<div class="d-flex justify-content-end w-25">
@@ -1098,8 +1103,9 @@ button.btn.btn-flex.badge-custom-bg.w-100.justify-content-center.px-2.ms-3.downl
 																						 <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-semibold fs-7 w-150px py-4 badge-custom-bg" data-kt-menu="true">
 																								<div class="menu-item px-3">
 																									<a class="menu-link text-danger px-3" href="{{ route('customer.files.download', ['id' => $file->id,'folder_name'=>$folder->name]) }}">Download File</a>
-																								{{-- <a class="menu-link text-danger px-3" onclick="confirmDelete({{ $file->id }}, '{{ $folder->name }}')">Delete</a> --}}
-																									 </div>
+																								  @if($file->Writer != 'Admin')
+																									<a class="menu-link text-danger px-3" onclick="confirmDelete({{ $file->id }}, '{{ $folder->name }}')">Delete</a>
+																								@endif	 </div>
 
 																								{{-- <div class="menu-item px-3">
 
@@ -1135,7 +1141,7 @@ button.btn.btn-flex.badge-custom-bg.w-100.justify-content-center.px-2.ms-3.downl
 
 										<!--end:::Tab pane-->
 										<div class="tab-pane fade" id="kt_ecommerce_customer_managePages" role="tabpanel">
-										     @if($hasSubscription && $totalPages > 0)
+										     @if($hasSubscription && $totalPages > 0 && $subscription->status === 'Active')
 											<div class="card my-10 card-custom-bg">
 												<div class="row p-5 mb-5">
 													<div class="col-md-12">
@@ -8569,6 +8575,8 @@ $(document).ready(function() {
 
 @else
 
+
+// $('.RemainingPages').text('N/A');
 
 var total_pages = {{ $used_subscription->total_pages ?? 0 }};
 	var remaining_pages = {{ $used_subscription->remaining_pages ?? 0 }};
