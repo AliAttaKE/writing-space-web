de82825b5451becd41c18c1fd6e2667bdf36c03f
Mohammad Raheel <jamers786@gmail.com>
2025-07-12 13:20:17 +0500

    Captcha

--- resources/views/frontend_final/signup.blade.php
+++ resources/views/frontend_final/signup.blade.php
@@ -10,7 +10,7 @@
                     <h1 class="heading gradient-text-2 text-center pb-5">
                         Signup
                     </h1>
-                    
+
                       <div class="mb-3 ">
 
                         <input type="text" name="name" placeholder="Name" class="w-100" value="{{ old('name') }}" required
@@ -22,7 +22,7 @@
 
                     </div>
                     <div class="mb-3">
-                      
+
                         <input type="email" name="email" placeholder="Email" class="w-100" value="{{ old('email') }}">
                         @error('email')
                               <span class="text-danger">{{ $message }}</span>
@@ -30,7 +30,7 @@
 
                     </div>
                     <div class="mb-3 password-block">
-                      
+
                             <input type="password"  class="w-100" id="create-password"  name="password" placeholder="Create Password">
                             @error('password')
                                   <span class="text-danger">{{ $message }}</span>
@@ -48,7 +48,7 @@
                         </div>
                     </div>
                     <div class="mb-3 password-block">
-    
+
                             <input type="password" class="w-100" name="password_confirmation" id="confirm-password" placeholder="Confirm Password">
                             @error('password_confirmation')
                                   <span class="text-danger">{{ $message }}</span>
@@ -58,11 +58,16 @@
                         </span>
                         <p id="password-match" class="password-match my-2"></p>
                     </div>
+                    @if (session('error'))
+    <div class="alert alert-danger">
+        {{ session('error') }}
+    </div>
+@endif
                     <div class="mb-3 text-center">
                         <button type="submit" class="gradient-button fw-bold login-button w-100">Signup</button>
 
 
-                    
+
                     </div>
 
                     <div class="mb-3 text-center fw-bold text-white">
@@ -83,6 +88,8 @@
                                 <a href="{{ route('google.login') }}" class="fw-bold text-white mb-0 d-flex align-items-center ms-3"><i class="fa-brands fa-light me-2 fa-google"></i><span class="icon-size text-capitalize" style="cursor:pointer;">Login with Google</span></a>
                             </div>
                         </div>
+                                    <input type="hidden" name="recaptcha_token" id="recaptcha_token">
+
                         <div class="col-md-6">
                             <div class="d-flex justify-content-center bordered-card py-3 px-3">
                                 <img src="{{ asset('fronted_final/assets/images/microsoft.png')}}" alt="" style="height: 32px; width: 32px;">
@@ -96,7 +103,14 @@
             </div>
         </div>
 
-        
+        <script src="https://www.google.com/recaptcha/api.js?render={{ env('RECAPTCHA_SITE_KEY') }}"></script>
+<script>
+    grecaptcha.ready(function () {
+        grecaptcha.execute('{{ env('RECAPTCHA_SITE_KEY') }}', {action: 'register'}).then(function (token) {
+            document.getElementById('recaptcha_token').value = token;
+        });
+    });
+</script>
    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
    <script>
        $(document).ready(function () {
@@ -123,10 +137,10 @@
 
            if (accessToken) {
 
-              
+
                // Using jQuery for simplicity, make sure to include the jQuery library
                $.ajax({
-               
+
                    url: '{{ route('microsoft.handle.ajax') }}',
                    type: 'GET',
                    data: { access_token: accessToken },
