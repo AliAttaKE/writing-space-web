ed78c7c68943b6981779d7f6439a60e2801e0eb9
shariqiqbal571 <shariqiqbal571@gmail.com>
2025-07-27 02:23:14 +0500

    check

--- resources/views/backend/customer/orderManagement/order_detail.blade.php
+++ resources/views/backend/customer/orderManagement/order_detail.blade.php
@@ -8212,7 +8212,7 @@ function submit_payment() {
         document.getElementById("kt_modal_upload_form1").addEventListener("submit", function(e) {
 
     const input = document.getElementById("file-3");
-    const allowedTypes = ["DOCX","docx", "pdf", "txt", "rtf","xlsx", "csv","pptx","jpeg", "png", "gif","jpg"];
+    const allowedTypes = ["DOCX","docx", "pdf", "txt", "rtf","xlsx", "csv","pptx","jpeg", "png", "gif","jpg", "zip", "rar"];
     const maxSizeMB = 500; // maximum size per file in MB
     let hasInvalid = false;
 
