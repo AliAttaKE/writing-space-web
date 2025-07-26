207fea9434a3d9a634e68dd53c76f0286974c073
shariqiqbal571 <shariqiqbal571@gmail.com>
2025-07-27 02:15:08 +0500

    check

--- resources/views/backend/customer/orderManagement/order_detail.blade.php
+++ resources/views/backend/customer/orderManagement/order_detail.blade.php
@@ -8409,7 +8409,7 @@ $(document).ready(function() {
 
 $('#media').on('change', function () {
   const allFiles = Array.from(this.files);
-  const allowedExts = ['docx', 'pdf', 'txt', 'rtf', 'xlsx', 'csv', 'pptx', 'jpeg', 'jpg'];
+  const allowedExts = ['docx', 'pdf', 'txt', 'rtf', 'xlsx', 'csv', 'pptx', 'jpeg', 'jpg','zip','rar'];
   const maxSize = 50 * 1024 * 1024; // 500MB
   let validFiles = [];
 
