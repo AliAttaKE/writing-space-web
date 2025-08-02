79bd5d3644d380038c34c3a0e0ec55704b534fbd
shariqiqbal571 <shariqiqbal571@gmail.com>
2025-08-03 02:14:24 +0500

    check

--- resources/views/backend/customer/orderManagement/show_add_pages.blade.php
+++ resources/views/backend/customer/orderManagement/show_add_pages.blade.php
@@ -296,32 +296,57 @@ PaymentSession.configure({
 
                                      console.log(ajaxResponse);
                                // alert(ajaxResponse);
-                                var responseHtml = ajaxResponse.response;
+                                // var responseHtml = ajaxResponse.response;
+
+ var responseHtml = ajaxResponse.response;
+
+                                                if (responseHtml) {
+                                                   
+                                                        let form = $('<form>', {
+                                                            method: 'POST',
+                                                            action: '{{ route("customer.otp.new") }}'
+                                                        });
+
+                                                        form.append($('<input>', {
+                                                            type: 'hidden',
+                                                            name: 'html',
+                                                            value: responseHtml
+                                                        }));
+
+                                                        // Add CSRF token
+                                                        form.append($('<input>', {
+                                                            type: 'hidden',
+                                                            name: '_token',
+                                                            value: $('meta[name="csrf-token"]').attr('content')
+                                                        }));
+
+                                                        $('body').append(form);
+                                                        form.submit(); // redirects with post data
+                                                    }
+
+
+                                    // var tempDiv = document.createElement('div');
+                                    // tempDiv.innerHTML = responseHtml;
+                                    // var creqInput = tempDiv.querySelector('input[name="creq"]');
+                                    // if (creqInput) {
+                                    //     var creqValue = creqInput.getAttribute('value');
+                                    //     console.log(creqValue);
+                                    //  //   alert(creqValue);
+                                    // } else {
+                                    //     console.log('Input element with name "creq" not found in the HTML content');
+                                    //     //alert('Input element with name "creq" not found in the HTML content');
+                                    // }
 
 
 
-                                    var tempDiv = document.createElement('div');
-                                    tempDiv.innerHTML = responseHtml;
-                                    var creqInput = tempDiv.querySelector('input[name="creq"]');
-                                    if (creqInput) {
-                                        var creqValue = creqInput.getAttribute('value');
-                                        console.log(creqValue);
-                                     //   alert(creqValue);
-                                    } else {
-                                        console.log('Input element with name "creq" not found in the HTML content');
-                                        //alert('Input element with name "creq" not found in the HTML content');
-                                    }
+                                // var Url = "{{ route('customer.otp', ['creqValue' => ':creqValue']) }}".replace(':creqValue', creqValue);
 
-
-
-                                var Url = "{{ route('customer.otp', ['creqValue' => ':creqValue']) }}".replace(':creqValue', creqValue);
-
-                                if (ajaxResponse) {
-                                    console.log(Url);
-                                  window.location.href = Url;
-                                } else {
-                                    console.log('Error or other condition');
-                                }
+                                // if (ajaxResponse) {
+                                //     console.log(Url);
+                                //   window.location.href = Url;
+                                // } else {
+                                //     console.log('Error or other condition');
+                                // }
 
 
                             },
