89554fe54017f609de880a63c9fb52e0baefb509
shariqiqbal571 <shariqiqbal571@gmail.com>
2025-08-16 00:28:37 +0500

    complete order set

--- resources/views/backend/admin/orderManagement/completed_order.blade.php
+++ resources/views/backend/admin/orderManagement/completed_order.blade.php
@@ -459,6 +459,8 @@
                                 </th> --}}
                                 <th class="min-w-70px fw_800 pb-8">order No</th>
                                 <th class="min-w-50px fw_800 pb-8">Topic</th>
+                                 <th class="min-w-80px fw_800 pb-8">Customer Name</th>
+                                 <th class="min-w-80px fw_800 pb-8">Email</th>
                                 <th class="min-w-70px fw_800 pb-8">Pages</th>
                                 <th class="min-w-70px fw_800 pb-8">Order Date</th>
                                 <th class="min-w-80px fw_800 pb-8">Due Date</th>
@@ -479,6 +481,8 @@
     </td> --}}
     <td><a class="fs-color-yellow" href="{{route('admin.admin-order-detail',[$o->order_id])}}">{{$o->order_id}}</a></td>
     <td class="limit-text">{{$o->subject}}</td>
+        <td class="limit-text">{{$o->user_name}}</td>
+                                                     <td class="">{{$o->user_email}}</td>
 
     <td>{{$o->number_of_pages}}</td>
     <td>{{ \Carbon\Carbon::parse($o->created_at)->format('d F Y h:iA')  }}</td>
