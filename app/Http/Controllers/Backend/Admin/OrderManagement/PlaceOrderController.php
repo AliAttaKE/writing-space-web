f77a18c3dd69b3c623502edf9af679538b43c2f4
shariqiqbal571 <shariqiqbal571@gmail.com>
2025-08-16 00:41:46 +0500

    delivered order set

--- app/Http/Controllers/Backend/Admin/OrderManagement/PlaceOrderController.php
+++ app/Http/Controllers/Backend/Admin/OrderManagement/PlaceOrderController.php
@@ -581,7 +581,9 @@ class PlaceOrderController extends Controller
     {
         $id = Auth()->user()->id;
         // $order=Orders::where('order_status','Delivered')->get();
-        $order = Orders::where('order_status', 'Delivered')
+        $order = Orders::
+        join('users', 'orders.user_id', '=', 'users.id') // Join with the users table
+        ->select('orders.*', 'users.name as user_name','users.email as user_email')->where('order_status', 'Delivered')
             ->when($request->order_id != null, function ($q) use ($request) {
                 return $q->where('order_id', $request->order_id);
             })
