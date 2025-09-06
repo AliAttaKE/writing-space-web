6a39b1791a21b3d645da759cf25453cb4367fc42
shariqiqbal571 <shariqiqbal571@gmail.com>
2025-09-07 00:43:20 +0500

    check

--- config/mail.php
+++ config/mail.php
@@ -46,12 +46,13 @@ return [
             'local_domain' => env('MAIL_EHLO_DOMAIN'),
 
              'stream' => [
-        'ssl' => [
-            'allow_self_signed' => true,
-            'verify_peer' => false,
-            'verify_peer_name' => false,
-        ],
+    'ssl' => [
+        'allow_self_signed' => true,
+        'verify_peer' => false,
+        'verify_peer_name' => false,
     ],
+],
+
         ],
 
         'ses' => [
