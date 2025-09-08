e5187a7e2fbfe815582e809faf184bba9cb99d77
shariqiqbal571 <shariqiqbal571@gmail.com>
2025-09-09 00:12:44 +0500

    check email

--- resources/views/emails/reset_password_link.blade.php
+++ resources/views/emails/reset_password_link.blade.php
@@ -1,19 +1,14 @@
 <!doctype html>
 <html lang="en">
-
 <head>
     <title>Password Reset Request</title>
-    <!-- Required meta tags -->
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <!-- Bootstrap CSS v5.3.2 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"/>
 </head>
-
 <body>
     <main class="container my-5">
-        <h1>Password Reset Request</h1>
+        {{-- <h1>Password Reset Request</h1> --}}
         <p>Dear Customer,</p>
         <p>We received a request to reset your password for your Writing-Space account. Please click the link below to reset your password:</p>
         <p>
@@ -21,17 +16,9 @@
                 Reset Password
             </a>
         </p>
+        <p>This link will expire in one hour for your security.</p>
         <p>If you did not request this password reset, please ignore this email or contact support if you have questions.</p>
         <p>Best regards,<br>Writing-Space Support Team</p>
     </main>
-
-    <!-- Optional JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
-        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous">
-    </script>
 </body>
-
 </html>
