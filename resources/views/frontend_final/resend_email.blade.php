3fc45635838d504a42495598e13e4215382ebcd1
shariqiqbalinnovative <shariq.m.iqbal@innovative-net.com>
2025-10-28 01:40:48 +0500

    chk

--- resources/views/frontend_final/resend_email.blade.php
+++ resources/views/frontend_final/resend_email.blade.php
@@ -58,7 +58,7 @@ button:disabled {
  <section class="bg-dark section-card-phases pt-160px">
         <div class="container d-flex justify-content-center mb-5">
             <div class="bordered-card p-5 col-md-10 forms-custom login-signup-form">
-    <img src="{{ asset('verify-email-banner.png') }}" alt="Verify Email Banner" width="320" style="margin-bottom: 25px;">
+    <!-- <img src="{{ asset('verify-email-banner.png') }}" alt="Verify Email Banner" width="320" style="margin-bottom: 25px;"> -->
 
     <h1 style="
     margin-left: 240px;
