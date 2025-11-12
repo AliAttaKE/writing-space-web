50e7cf3bf0c2918d8998ffa122894cdb2d896b2f
shariqiqbalinnovative <shariq.m.iqbal@innovative-net.com>
2025-11-12 22:56:03 +0500

    chk

--- resources/views/backend/admin/customer_orders/index.blade.php
+++ resources/views/backend/admin/customer_orders/index.blade.php
@@ -398,28 +398,30 @@ $('.edit-order').on('click', function() {
     hiddenInput.value = select.value;
 });
 
-    // Edit order form submission
     $('#editOrderForm').on('submit', function(e) {
-        e.preventDefault();
-        var formData = $(this).serialize();
+    e.preventDefault();
 
-        $.ajax({
-            url: '{{ route("admin.customer_orders.update") }}',
-            type: 'POST',
-            data: formData,
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            },
-            success: function(response) {
-                Swal.fire('Success!', response.success, 'success');
-                $('#editCustomerOrderModal').modal('hide');
-                location.reload();
-            },
-            error: function(xhr) {
-                var error = xhr.responseJSON.error;
-                Swal.fire('Error!', error, 'error');
-            }
-        });
+    // Set hidden input from readonly input
+    $('#editCustomerNameInput').val($('#editCustomerName').val());
+
+    var formData = $(this).serialize();
+
+    $.ajax({
+        url: '{{ route("admin.customer_orders.update") }}',
+        type: 'POST',
+        data: formData,
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        },
+        success: function(response) {
+            Swal.fire('Success!', response.success, 'success');
+            $('#editCustomerOrderModal').modal('hide');
+            location.reload();
+        },
+        error: function(xhr) {
+            var error = xhr.responseJSON.error;
+            Swal.fire('Error!', error, 'error');
+        }
     });
 });
 
