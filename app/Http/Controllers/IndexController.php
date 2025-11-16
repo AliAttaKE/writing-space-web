0dbff4ca22848e93976a3caf1f0f455f7b3ea089
shariqiqbalinnovative <shariq.m.iqbal@innovative-net.com>
2025-11-17 00:25:44 +0500

    chk

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -228,6 +228,7 @@ class IndexController extends Controller
 
     return response()->json(['message' => 'Verification email sent successfully']);
 }
+
    public function emailverified()
    {
         return view('frontend_final.email_verified');
